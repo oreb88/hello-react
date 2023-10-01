665d75f4009dd2201df3665f40b9b5654833eb37
oreb <javabt01@gmail.com>
2023-10-01 23:04:41 +0900

    map & filter

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import ScrollBox from "./ScrollBox";
 import {Component} from "react";
+import IterationSample from "./IterationSample";
 
 class MyApp extends Component {
 
@@ -8,8 +9,10 @@ class MyApp extends Component {
         return (
             <div>
                 {/*scrollBox 라는 변수에 ScrollBox 컴포넌트 주소를 저장.*/}
-                <ScrollBox ref={(ref) => this.scrollBox = ref}/>
-                <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+                {/*<ScrollBox ref={(ref) => this.scrollBox = ref}/>*/}
+                {/*<button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>*/}
+
+                <IterationSample/>
             </div>)
     };
 }
